b69ced9b651df26b4722c5ea1c5d02bd25b2177b
DannyNoe <dannymnoe@gmail.com>
2020-12-09 18:41:59 -0500

    Gram frontend

---
+++ UwU++_frontend.py
@@ -0,0 +1,14 @@
+# grammar for UwU++
+uwupp_lex = "UwU++_lex.py"
+from ply import yacc
+from uwupp_lex import tokens, lexer
+
+# set precedence and associativity
+# NOTE: all arithmetic operator need to have tokens
+#       so that we can put them into the precedence table
+precedence = (
+              ('left', 'EQ', 'LE'),
+              ('left', 'PLUS', 'MINUS'),
+              ('left', 'TIMES', 'DIVIDE'),
+              ('right', 'UMINUS', 'NOT')
+             )

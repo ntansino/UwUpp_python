549434e02d76a08f1f1446a68685dc625885e99d
DannyNoe <dannymnoe@gmail.com>
2020-12-09 14:52:17 -0500

    Fixed reserved words.

--- UwU++_lex.py
+++ UwU++_lex.py
@@ -4,20 +4,20 @@ import re
 from ply import lex
 
 reserved = {
-    'iws'     : 'IWS',
-    'nyaa'    : 'NYAA',
     'gweatew twan' : 'GWEATEW TWAN',
-    'wess twan' : 'WESS TWAN',
-    'eqwall twoo' : 'EQWALL TWOO',
-    'notices' : 'NOTICES',
-    'ewse' : 'EWSE'
-    'OwO' : 'OWO',
-    'stawp' : 'STAWP',
-    'pwus' : 'PWUS',
-    'twimes' : 'TWIMES',
-    'diwide' : 'DIWIDE',
-    'nuzzels' : 'NUZZELS',
-    'wetuwn' : 'WETUWN',
-    'decwawe' : 'DECWAWE',
-    'nowt' : 'NOWT'
+    'wess twan'    : 'WESS TWAN',
+    'eqwall twoo'  : 'EQWALL TWOO',
+    'iws'          : 'IWS',
+    'nyaa'         : 'NYAA',
+    'notices'      : 'NOTICES',
+    'ewse'         : 'EWSE',
+    'OwO'          : 'OWO',
+    'stawp'        : 'STAWP',
+    'pwus'         : 'PWUS',
+    'twimes'       : 'TWIMES',
+    'diwide'       : 'DIWIDE',
+    'nuzzels'      : 'NUZZELS',
+    'wetuwn'       : 'WETUWN',
+    'decwawe'      : 'DECWAWE',
+    'nowt'         : 'NOWT'
 }
